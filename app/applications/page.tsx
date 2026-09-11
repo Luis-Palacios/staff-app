35d288219d2c5a67e97544da89633c607c31e929
Luis Palacios <lrpalaciosdev@gmail.com>
2026-09-10 20:18:36 -0600

    add initial api call

--- app/applications/page.tsx
+++ app/applications/page.tsx
@@ -1,6 +1,11 @@
 import { title } from "@/components/primitives";
 
-export default function ApplicationsPage() {
+export default async function ApplicationsPage() {
+  const response = await fetch("http://localhost:8000/applications/recents");
+  const data = await response.json();
+
+  console.log(data);
+
   return (
     <div>
       <h1 className={title()}>Applications</h1>
