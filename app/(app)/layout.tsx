ef0e527c54b42458234828b6602229ed0a863fdf
Luis Palacios <lrpalaciosdev@gmail.com>
2026-09-17 15:24:23 -0600

    forbid access to pending role

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import { getServerSession } from "@/api/auth-api/helpers/get-server-session";
 import { AppShell } from "@/components/app-shell";
 import { UserProvider } from "@/lib/contexts/user-context";
+import { AuthRole } from "@/lib/auth/roles";
 
 export default async function AppLayout({
   children,
@@ -17,6 +18,10 @@ export default async function AppLayout({
 
   if (!session.user.emailVerified) {
     redirect("/needs-verification");
+  }
+
+  if (session.user.role && session.user.role === AuthRole.Pending) {
+    redirect("/needs-role");
   }
 
   return (
