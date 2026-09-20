2dbb1ca8d7ec945c314bc278a28bad07a774e788
Luis Palacios <lrpalaciosdev@gmail.com>
2026-09-19 19:02:09 -0600

    add pending applications count on dashboard

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,9 +1,27 @@
-export default function Home() {
-  return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-xl text-center justify-center" />
+import { Card } from "@heroui/react/card";
+import { Link } from "@heroui/react";
 
-      <div className="mt-8" />
+import { getRecentApplicationsCount } from "@/api/applications-membership-api/client";
+
+export default async function Home() {
+  const recentApplicationsCount = await getRecentApplicationsCount();
+
+  return (
+    <section className="gap-4 py-2 md:py-2 grid grid-cols-1 md:grid-cols-4">
+      <Card className="col-span-1" variant="secondary">
+        <Card.Header>
+          <Card.Title className="mb-4">Recent Applications</Card.Title>
+          <Card.Description className="mb-4 !text-warning text-3xl">
+            {recentApplicationsCount}
+          </Card.Description>
+        </Card.Header>
+        <Card.Footer>
+          <Link className="text-accent decoration-accent" href="/applications">
+            View All
+            <Link.Icon />
+          </Link>
+        </Card.Footer>
+      </Card>
     </section>
   );
 }
